1ebfcb4a5a86654b2d58e3aa8ab40f1e9ee6bcc7
MacDonald91 <alasdairmacdonald91@gmail.com>
2026-03-31 22:18:57 +0100

    Level 100 complete - display all episodes

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
@@ -6,7 +5,29 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  // Clear existing content
+  rootElem.innerHTML = "";
+
+  episodeList.forEach(function(episode) {
+
+    const season = String(episode.season).padStart(2, "0");
+    const number = String(episode.number).padStart(2, "0");
+    const episodeCode = `S${season}E${number}`;
+
+    const episodeDiv = document.createElement("div");
+
+    episodeDiv.innerHTML = `
+  <h2>${episode.name}</h2>
+  <p>${episodeCode}</p>
+  <p>Season: ${episode.season}</p>
+  <p>Episode: ${episode.number}</p>
+  <img src="${episode.image.medium}" />
+  <p>${episode.summary}</p>
+`;
+
+    rootElem.appendChild(episodeDiv);
+  });
 }
 
 window.onload = setup;
